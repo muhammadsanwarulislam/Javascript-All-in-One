5acf1eaaf473206dbd33d839c9d006b4825500b9
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2022-05-15 13:44:43 +0600

    working on data structures and algorith section

---
+++ DOM Manipulation/Event Listeners the Event Object/app.js
@@ -0,0 +1,8 @@
+document.querySelector('.clear-tasks').addEventListener('mousehover',onClick)
+
+function onClick(e){
+    let val
+    val = e
+    console.log(val)
+}
+
---
+++ Data Structures and Algorithms/Sum of n natural numbers/app.js
@@ -0,0 +1,27 @@
+//Add up to number slower
+// function addUpTo(n){
+//     let total = 0
+
+//     for(let i=1; i<=n; i++)
+//         total += i
+
+//     return total
+// }
+
+// let t1 = performance.now()
+// console.log(addUpTo(1000000000))
+// let t2 = performance.now()
+// console.log(`Time Elapsed : ${t2 - t1} seconds`)
+
+//Add up to faster
+function addUpTo(n){
+    return n * (n +1) /2
+}
+
+let t1 = performance.now()
+console.log(addUpTo(1000000000))
+let t2 = performance.now()
+console.log(`Time Elapsed : ${t2 - t1} seconds`)
+
+
+
