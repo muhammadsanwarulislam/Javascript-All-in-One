ec2ff3c069c79f6cafd74df499436c9007b0b419
Muhammad Sanwarul Islam <muhammad.sanwarul94@gmail.com>
2022-06-12 13:22:41 +0600

    Update README.md

--- React/centerpoint/src/index.js
+++ React/centerpoint/src/index.js
@@ -4,35 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function getOrange(user) {
-  if(user) {
-    return <h1>Hello {user}</h1>
-  }
-  return <h2>Hello stanger</h2>
-}
-const element = <h1>{getOrange('muhammad')}</h1>
-
-const swapNum = (function(array,num1,num2){
-  let tmp = array[num1]
-    array[num1] = array[num2]
-    array[num2] = tmp
-    return (
-      <React.StrictMode>
-      <App />
-      <h1>{array}</h1>
-    </React.StrictMode>
-    )
-})([10,2,1,6,3],2,3);
-//Outpul: x10,2,6,1,3
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(swapNum,
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
+setInterval(tick, 1000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
